eb3fccdd312a49c339b3631e0da14ba925a22549
MaximilianZeng <maximilianzeng@MaximilianZengs-MacBook-Pro.local>
2020-03-26 22:03:48 -0400

    Update 03/26

--- BigQuery_Posts.sql
+++ BigQuery_Posts.sql
@@ -1,7 +1,10 @@
 -- Declare variables to use in later queries
 DECLARE target_site STRING;
-SET target_site = 'bizstandardnews.com';
+DECLARE target_reddit_group STRING;
+SET target_reddit_group = 'The_Donald';
+SET target_site = 'abcnews.com.co';
 
+-- search for posts
 WITH tables AS (
 	-- SELECT * FROM `fh-bigquery.reddit_posts.2019*`
 	SELECT * FROM `fh-bigquery.reddit_posts.2019_04` UNION ALL
@@ -10,28 +13,55 @@ WITH tables AS (
 	SELECT * FROM `fh-bigquery.reddit_posts.2019_07` UNION ALL
 	SELECT * FROM `fh-bigquery.reddit_posts.2019_08` 
 )
-
-
 -- Scan for posts linking to target websites from selected months
-SELECT id, title, selftext, subreddit, author, created_utc, url, num_comments, score, ups, downs
+SELECT id, title, selftext, subreddit, author, created_utc, url, num_comments, score
 FROM tables
+-- search if the title/comment contain the target site
 WHERE title LIKE CONCAT('%', target_site, '%')
-  OR selftext LIKE CONCAT('%', target_site, '%')
+    OR selftext LIKE CONCAT('%', target_site, '%')
 LIMIT 100
 
 
+-- # posts in specific subreddit
+SELECT COUNT(id) as total_post
+FROM tables
+WHERE subreddit LIKE CONCAT('%', target_reddit_group, '%')
 
-WITH tables AS (
-	SELECT * FROM `fh-bigquery.reddit_comments.2019*`
+-- # posts linking to target websites in specific subreddit
+SELECT COUNT(id) as post_contain_links
+FROM tables
+WHERE subreddit LIKE CONCAT('%', target_reddit_group, '%')
+    AND (
+        title LIKE CONCAT('%', 'http', '%')
+        OR title LIKE CONCAT('%', 'www', '%')
+        OR selftext LIKE CONCAT('%', 'http', '%')
+        OR selftext LIKE CONCAT('%', 'www', '%')
+    )
+
+-- # posts containing bad links
+SELECT COUNT(id) as post_contain_poison_links
+FROM tables
+WHERE subreddit LIKE CONCAT('%', target_reddit_group, '%')
+    AND (
+        title LIKE CONCAT('%', target_site, '%')
+        OR selftext LIKE CONCAT('%', target_site, '%')
+    )
+
+
+
+
+
+-- Search for comments
+-- WITH tables AS (
+-- 	SELECT * FROM `fh-bigquery.reddit_comments.2019*`
 -- 	SELECT * FROM `fh-bigquery.reddit_posts.2019_04` UNION ALL
 -- 	SELECT * FROM `fh-bigquery.reddit_posts.2019_05` UNION ALL
 -- 	SELECT * FROM `fh-bigquery.reddit_posts.2019_06` UNION ALL
 -- 	SELECT * FROM `fh-bigquery.reddit_posts.2019_07` UNION ALL
 -- 	SELECT * FROM `fh-bigquery.reddit_posts.2019_08` 
-)
-
+-- )
 -- Scan for comments linking to target websites from selected months
-SELECT id, body, subreddit, author, created_utc, score, ups, downs
-FROM tables
-WHERE body LIKE CONCAT('%', target_site, '%')
-LIMIT 100
+-- SELECT id, body, subreddit, author, created_utc, score, ups, downs
+-- FROM tables
+-- WHERE body LIKE CONCAT('%', target_site, '%')
+-- LIMIT 100
--- BigQuery_User.sql
+++ BigQuery_User.sql
@@ -1,6 +1,11 @@
 DECLARE target_user STRING;
+DECLARE target_site STRING;
+DECLARE target_reddit_group STRING;
 
 SET target_user = ;
+SET target_reddit_group = 'The_Donald';
+SET target_site = 'abcnews.com.co';
+
 
 WITH post_tables AS (
   -- SELECT * FROM `fh-bigquery.reddit_posts.2019*`
@@ -13,17 +18,51 @@ WITH post_tables AS (
 -- Select one user's activity in reddit comments in particular subreddit
 SELECT
 url,  
-  comments.id as Comment_ID, 
-  comments.body as Comment_Text,
-  comments.author as Comment_Author, 
-  comments.link_id as Comment_LinkID, 
-  comments.parent_id as Comment_ParentID
+    comments.id as Comment_ID, 
+    comments.body as Comment_Text,
+    comments.author as Comment_Author, 
+    comments.link_id as Comment_LinkID, 
+    comments.parent_id as Comment_ParentID
 FROM post_tables
 LEFT_JOIN comment_tables
 ON (
-  posts.subreddit_id = comments.subreddit_id
-  AND comments.link_id LIKE CONCAT('%', parent_id, '%')
+    posts.subreddit_id = comments.subreddit_id
+    AND comments.link_id LIKE CONCAT('%', parent_id, '%')
 )
-WHERE author ='Spartan_Marine' 
-AND subreddit in ('deathguard40k')
+WHERE author =target_user
+AND subreddit = target_reddit_group
+-- AND subreddit in ('deathguard40k')
 LIMIT 100
+
+-- # posts in specific subreddit by target user
+SELECT id, title, selftext, subreddit, author, created_utc, url, num_comments, score
+-- SELECT COUNT(id) as post_contain_poison_links
+FROM tables
+WHERE subreddit = target_reddit_group
+-- WHERE subreddit in (target_reddit_group, )
+    AND author = target_user
+
+-- # posts linking to a websites in specific subreddit by target user
+SELECT id, title, selftext, subreddit, author, created_utc, url, num_comments, score
+-- SELECT COUNT(id) as post_contain_poison_links
+FROM tables
+WHERE subreddit = target_reddit_group
+    AND (
+        title LIKE CONCAT('%', 'http', '%')
+        OR title LIKE CONCAT('%', 'www', '%')
+        OR selftext LIKE CONCAT('%', 'http', '%')
+        OR selftext LIKE CONCAT('%', 'www', '%')
+    )
+    AND author = target_user
+
+    -- # posts linking to target websites in specific subreddit by target user
+-- # posts containing bad links
+SELECT id, title, selftext, subreddit, author, created_utc, url, num_comments, score
+-- SELECT COUNT(id) as post_contain_poison_links
+FROM tables
+WHERE subreddit = target_reddit_group
+    AND (
+        title LIKE CONCAT('%', target_site, '%')
+        OR selftext LIKE CONCAT('%', target_site, '%')
+    )
+    AND author = target_user
